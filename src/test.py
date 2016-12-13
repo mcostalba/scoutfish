20f18a790790dfbee3a01b3ff2525e65794d115f
Marco Costalba <Mcostalba@gmail.com>
2016-12-13 08:27:01 +0000

    Use SHA1 to verify test queries
    Instead of checking just the number of matches for each query, now
    check against a SHA1 hash of the full match info, including offsets
    and plies.
    
    This is more powerful because we can spot a wrong result
    even if the number of matches remains the same but it changes
    just one offset or one ply.
    
    Also run pep8 on it

--- src/test.py
+++ src/test.py
@@ -1,58 +1,67 @@
 #!/usr/bin/env python
 
 import argparse
+import hashlib
 import json
 import os
 import sys
 
 from scoutfish import Scoutfish
 
-QUERY_DB = [
-    {'q': { "sub-fen": "8/8/p7/8/8/1B3N2/8/8" },                     'matches': 29},
-    {'q': { "sub-fen": "8/8/8/8/1k6/8/8/8", "result": "1/2-1/2" },   'matches':  1},
-    {'q': { "sub-fen": ["8/8/8/q7/8/8/8/8", "8/8/8/r7/8/8/8/8"] },   'matches': 72},
-    {'q': { "material": "KQRRBNPPPPKQRRNNPPPP", "stm": "BLACK" },    'matches':  2},
-    {'q': { "material": "KQRRBNNPPPPKQRRBNNPPPP", "result": "0-1" }, 'matches':  2},
-    {'q': { "material": ["KRBPPPKRPPP", "KRPPPKRPPP"] },             'matches':  4},
-    {'q': { "white-move": "Nb7" },                                   'matches':  6},
-    {'q': { "black-move": "c3" },                                    'matches': 27},
-    {'q': { "black-move": "e1=Q" },                                  'matches':  1},
-    {'q': { "black-move": "O-O" },                                   'matches':354},
-    {'q': { "black-move": "O-O-O" },                                 'matches': 28},
-    {'q': { "black-move": ["O-O-O", "O-O"] },                        'matches':382},
-    {'q': { "white-move": ["a7", "b7"] },                            'matches': 16},
+QUERIES = [
+    {'q': {'sub-fen': '8/8/p7/8/8/1B3N2/8/8'}, 'sig': '6265541'},
+    {'q': {'sub-fen': '8/8/8/8/1k6/8/8/8', 'result': '1/2-1/2'}, 'sig': '0369452'},
+    {'q': {'sub-fen': ['8/8/8/q7/8/8/8/8', '8/8/8/r7/8/8/8/8']}, 'sig': 'd0eaae9'},
+    {'q': {'material': 'KQRRBNPPPPKQRRNNPPPP', 'stm': 'BLACK'}, 'sig': 'e4caa92'},
+    {'q': {'material': 'KQRRBNNPPPPKQRRBNNPPPP', 'result': '0-1'}, 'sig': 'c892fb9'},
+    {'q': {'material': ['KRBPPPKRPPP', 'KRPPPKRPPP']}, 'sig': '377a2bb'},
+    {'q': {'white-move': 'Nb7'}, 'sig': '6089546'},
+    {'q': {'black-move': 'c3'}, 'sig': '8357cde'},
+    {'q': {'black-move': 'e1=Q'}, 'sig': '03f16ba'},
+    {'q': {'black-move': 'O-O'}, 'sig': 'ea3c83b'},
+    {'q': {'black-move': 'O-O-O'}, 'sig': '9f3fd79'},
+    {'q': {'black-move': ['O-O-O', 'O-O']}, 'sig': '40d4f99'},
+    {'q': {'white-move': ['a7', 'b7']}, 'sig': '295355b'},
 
-    {'q': { "sub-fen": ["rnbqkbnr/pp1p1ppp/2p5/4p3/3PP3/8/PPP2PPP/RNBQKBNR",
-                        "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R"] }, 'matches': 50},
+    {'q': {'sub-fen': ['rnbqkbnr/pp1p1ppp/2p5/4p3/3PP3/8/PPP2PPP/RNBQKBNR',
+                       'rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R']},
+     'sig': '4dace7c'},
 
-    {'q': { "sequence": [ { "sub-fen": "8/3p4/8/8/8/8/8/8" ,
-                            "result": "1-0" },
-                          { "sub-fen": "8/2q5/8/8/8/8/8/R6R" }] }, 'matches':  8},
+    {'q': {'sequence': [{'sub-fen': '8/3p4/8/8/8/8/8/8', 'result': '1-0'},
+                        {'sub-fen': '8/2q5/8/8/8/8/8/R6R'}]},
+     'sig': 'bda360d'},
 
-    {'q': { "sequence": [ { "sub-fen": "r1bqkb1r/pppp1ppp/2n2n2/1B2p3/"
-                                        "4P3/2N2N2/PPPP1PPP/R1BQK2R" },
-                          { "sub-fen": "8/8/8/8/2B5/8/8/8" },
-                          { "sub-fen": "8/8/8/8/8/5B2/8/8" } ] },  'matches':  2},
+    {'q': {'sequence': [{'sub-fen': 'r1bqkb1r/pppp1ppp/2n2n2/1B2p3/'
+                                    '4P3/2N2N2/PPPP1PPP/R1BQK2R'},
+                        {'sub-fen': '8/8/8/8/2B5/8/8/8'},
+                        {'sub-fen': '8/8/8/8/8/5B2/8/8'}]},
+     'sig': '7c244bb'},
 
-    {'q': { "streak": [ { "sub-fen": "r1bqkb1r/pppp1ppp/2n2n2/1B2p3/"
-                                      "4P3/2N2N2/PPPP1PPP/R1BQK2R"},
-                        { "result": "0-1"}, {"result": "0-1"} ] }, 'matches':  2},
+    {'q': {'streak': [{'sub-fen': 'r1bqkb1r/pppp1ppp/2n2n2/1B2p3/4P3/2N2N2/PPPP1PPP/R1BQK2R'},
+                      {'result': '0-1'}, {'result': '0-1'}]},
+     'sig': '68b4158'},
 
-    {'q': { "sequence": [ { "sub-fen": "rnbqkb1r/pp1p1ppp/4pn2/2pP4/2P5/2N5/PP2PPPP/R1BQKBNR"},
-                          { "streak": [ { "white-move": "e5"}, { "black-move": "dxe5"}, { "white-move": "f5"} ] },
-                          { "white-move": "Ne4"} ] },
-     'matches': 1},
+    {'q': {'sequence': [{'sub-fen': 'rnbqkb1r/pp1p1ppp/4pn2/2pP4/2P5/2N5/PP2PPPP/R1BQKBNR'},
+                        {'streak': [{'white-move': 'e5'}, {'black-move': 'dxe5'}, {'white-move': 'f5'}]},
+                        {'white-move': 'Ne4'}]},
+     'sig': '52706ff'},
 
-    {'q': { "sequence": [ { "sub-fen": "rnbqkb1r/pp1p1ppp/4pn2/2pP4/2P5/2N5/PP2PPPP/R1BQKBNR"},
-                          { "streak": [ { "white-move": "e5"}, { "black-move": "dxe5"}, { "white-move": "f5"},
-                                        { "white-move": "Ne4"} ] } ] },
-     'matches': 0},
+    {'q': {'sequence': [{'sub-fen': 'rnbqkb1r/pp1p1ppp/4pn2/2pP4/2P5/2N5/PP2PPPP/R1BQKBNR'},
+                        {'streak': [{'white-move': 'e5'}, {'black-move': 'dxe5'}, {'white-move': 'f5'},
+                                    {'white-move': 'Ne4'}]}]},
+     'sig': '97d170e'},
 
-    {'q': { "sequence": [ { "sub-fen": "rnbqkb1r/pp1p1ppp/4pn2/2pP4/2P5/2N5/PP2PPPP/R1BQKBNR"},
-                          { "streak": [ { "white-move": "e5"}, { "pass": ""}, { "white-move": "f5"} ] },
-                          { "white-move": "Ne4"} ] },
-     'matches': 1},
+    {'q': {'sequence': [{'sub-fen': 'rnbqkb1r/pp1p1ppp/4pn2/2pP4/2P5/2N5/PP2PPPP/R1BQKBNR'},
+                        {'streak': [{'white-move': 'e5'}, {'pass': ''}, {'white-move': 'f5'}]},
+                        {'white-move': 'Ne4'}]},
+     'sig': '52706ff'},
 ]
+
+
+def signature(matches):
+    d = str(matches)
+    sha = hashlib.sha1(d).hexdigest()
+    return sha[:7]
 
 
 if __name__ == "__main__":
@@ -82,14 +91,15 @@ if __name__ == "__main__":
 
     # Run queries
     cnt = 1
-    for e in QUERY_DB:
+    for e in QUERIES:
         sys.stdout.write('Query ' + str(cnt) + '...')
         sys.stdout.flush()
         result = p.scout(e['q'])
-        if (result['match count'] == e['matches']):
-            print('OK')
+        num = str(result['match count'])
+        if (e['sig'] == signature(result['matches'])):
+            print('OK (' + num + ')')
         else:
-            print('FAIL')
+            print('FAIL (' + num + ')')
         p.before = ''
         cnt += 1
 
